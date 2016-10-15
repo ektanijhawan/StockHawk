59fcfb609dc13844632fbd027dbffd6a7dd14582
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-10-15 21:46:41 +0530

    Initial commit

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -0,0 +1,47 @@
+package com.sam_chordas.android.stockhawk.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.TaskParams;
+import com.sam_chordas.android.stockhawk.rest.Utils;
+
+/**
+ * Created by sam_chordas on 10/1/15.
+ */
+public class StockIntentService extends IntentService {
+
+  public StockIntentService(){
+    super(StockIntentService.class.getName());
+  }
+
+  public StockIntentService(String name) {
+    super(name);
+  }
+
+  @Override protected void onHandleIntent(Intent intent) {
+    Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+    StockTaskService stockTaskService = new StockTaskService(this);
+    Bundle args = new Bundle();
+    if (intent.getStringExtra("tag").equals("add")){
+      args.putString("symbol", intent.getStringExtra("symbol"));
+    }
+    // We can call OnRunTask from the intent service to force it to run immediately instead of
+    // scheduling a task.
+    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+if(Utils.flag==true)
+  sendMessage();
+  }
+
+  private void sendMessage() {
+    Log.d("sender", "Broadcasting message");
+    Intent intent = new Intent("custom-event-name");
+    // You can also include some extra data.
+    intent.putExtra("message", "Invalid input.!");
+    LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+  }
+}
