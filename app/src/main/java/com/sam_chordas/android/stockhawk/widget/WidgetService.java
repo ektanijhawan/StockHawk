f91d76f84df5d74515a8da1ac5333015cf9f66a8
Ekta Nijhawan <ektanijhawan1@gmail.com>
2016-10-25 17:41:22 +0530

    Widget Added-Doesn't work

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetService.java
@@ -0,0 +1,157 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+/**
+ * Created by Ekta on 25-10-2016.
+ */
+public class WidgetService   extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StockRVFactory(this.getApplicationContext(), intent);
+    }
+
+
+    /**
+     * Equivalent to a CursorAdapter/ArrayAdapter with ListView.
+     */
+    public class StockRVFactory implements RemoteViewsFactory {
+
+        private final String TAG = StockRVFactory.class.getSimpleName();
+        private Context context;
+        private Cursor cursor;
+        private int appWidgetId;
+
+        public StockRVFactory(Context context, Intent intent) {
+            this.context = context;
+            this.appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+
+
+
+
+
+
+        @Override
+        public void onCreate() {
+
+            cursor = getContentResolver().query(
+                    QuoteProvider.Quotes.CONTENT_URI,
+                    new String[]{QuoteColumns._ID, //0
+                            QuoteColumns.SYMBOL, //1
+                            QuoteColumns.BIDPRICE, //2
+                            QuoteColumns.CHANGE, //3
+                            QuoteColumns.ISUP}, //4
+                    QuoteColumns.ISCURRENT + " = ?",
+                    new String[]{"1"},
+                    null
+            );
+        }
+
+        /**
+         *
+         * Called when notifyDataSetChanged() is called.
+         * Hence we can update the widget with new data!
+         */
+        @Override
+        public void onDataSetChanged() {
+            cursor = getContentResolver().query(
+                    QuoteProvider.Quotes.CONTENT_URI,
+                    new String[]{QuoteColumns._ID, //0
+                            QuoteColumns.SYMBOL, //1
+                            QuoteColumns.BIDPRICE, //2
+                            QuoteColumns.CHANGE, //3
+                            QuoteColumns.ISUP}, //4
+                    QuoteColumns.ISCURRENT + " = ?",
+                    new String[]{"1"},
+                    null
+            );
+        }
+
+        @Override
+        public void onDestroy() {
+            //close the cursor.
+            if (this.cursor != null)
+                this.cursor.close();
+        }
+
+        @Override
+        public int getCount() {
+            //Meta-function for the AppWidgetManager
+            return (this.cursor != null) ? this.cursor.getCount() : 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            RemoteViews remoteViews = new RemoteViews(this.context.getPackageName(), R.layout.list_item_quote);
+
+            if (this.cursor.moveToPosition(position)) {
+                String symbol = cursor.getString(1);
+                remoteViews.setTextViewText(R.id.stock_symbol, symbol);
+                remoteViews.setTextViewText(R.id.bid_price, cursor.getString(2));
+                remoteViews.setTextViewText(R.id.change, cursor.getString(3));
+
+                if (cursor.getInt(4) == 1) {
+                    remoteViews.setInt(R.id.change, "setBackgroundResource",
+                            R.drawable.percent_change_pill_green);
+                } else {
+                    remoteViews.setInt(R.id.change, "setBackgroundResource",
+                            R.drawable.percent_change_pill_red);
+                }
+
+                Bundle extras = new Bundle();
+                extras.putString("symbol", symbol);
+
+                Intent fillInIntent = new Intent();
+                fillInIntent.putExtras(extras);
+                remoteViews.setOnClickFillInIntent(R.id.ll, fillInIntent);
+
+            }
+
+
+            return remoteViews;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            //we have only one type of view to display so returning 1.
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            //Return the data from the ID column of the table.
+            return this.cursor.getInt(0);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            /**
+             * As the table contains a column called ID,
+             * whose value we are returning at getItemId(),
+             * and also is a primary column,
+             * every Id is unique and hence stable.
+             */
+            return true;
+        }
+    }
+
+}
--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/widgetclass.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/widgetclass.java
@@ -1,37 +1,49 @@
 package com.sam_chordas.android.stockhawk.widget;
 
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.ui.StockDetailActivity;
 
 import java.net.URL;
+import java.util.Random;
 
 /**
  * Created by Ekta on 24-10-2016.
  */
 public class widgetclass extends AppWidgetProvider {
-public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for(int i=0; i<appWidgetIds.length; i++){
-        int currentWidgetId = appWidgetIds[i];
+        private static final String ACTION_CLICK = "ACTION_CLICK";
 
-            String url="";
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setData(Uri.parse(url));
+        @Override
+        public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                             int[] appWidgetIds) {
 
-        PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_my_stocks);
+                for (int i = 0; i< appWidgetIds.length ; i++){
 
-        views.setOnClickPendingIntent(R.id.ll, pending);
-        appWidgetManager.updateAppWidget(currentWidgetId,views);
-        Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
+                        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+                        //Update the widget
+                        views.setRemoteAdapter(R.id.lv_stock_widget_layout, new Intent(context, WidgetService.class));
+
+                        Intent intent = new Intent(context, StockDetailActivity.class);
+                        PendingIntent pendingIntent = TaskStackBuilder.create(context)
+                                .addNextIntentWithParentStack(intent)
+                                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        views.setPendingIntentTemplate(R.id.lv_stock_widget_layout, pendingIntent);
+
+                        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds[i], R.id.lv_stock_widget_layout);
+                        appWidgetManager.updateAppWidget(appWidgetIds[i], views);
+                }
         }
-        }
-        }
+}
